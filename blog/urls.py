cec841b6f665b8fdb56b6a9c4c20521d26cb956f
Ali seyfi <153763977+AisDisappeared@users.noreply.github.com>
2024-02-19 14:48:39 +0330

    changing the href attributes
    and set a name for each app

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path,include
 from .views import * 
 
+
 app_name = 'blog'
 
 urlpatterns = [
+    path('',blog_home_view,name='index'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def blog_home_view(request):
+    return render(request , 'blog/blog.html' )
