8265ef56639d5ac33eeac601fdc08c58e8ba53f7
Ali seyfi <153763977+AisDisappeared@users.noreply.github.com>
2024-02-19 14:35:26 +0330

    django templates inheritance
    used by base html page

--- website/urls.py
+++ website/urls.py
@@ -2,6 +2,8 @@ from django.urls import path,include
 from .views import * 
 
 
+app_name = 'website'
+
 urlpatterns = [
     path('',index_view,name='index'),
     path('about' , about_view ,name='about'),
