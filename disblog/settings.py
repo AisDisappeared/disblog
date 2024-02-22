1a32410333111192d800c0a1f06c936ef511bcc6
Ali seyfi <153763977+AisDisappeared@users.noreply.github.com>
2024-02-22 19:01:17 +0330

    setting of django smtp
    added

--- disblog/settings.py
+++ disblog/settings.py
@@ -195,3 +195,13 @@ AUTHENTICATION_BACKENDS = [
     'accounts.custom_auth_backends.CustomAuthenticationBackend',
     'django.contrib.auth.backends.ModelBackend',
 ]
+
+
+
+# django smtp email backend settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'Alihunter4640@gmail.com'
+EMAIL_HOST_PASSWORD = 'umewmbhmmzimbqmy'
