d9750139c24b276c127436cf24ee372954010203
Ali seyfi <153763977+AisDisappeared@users.noreply.github.com>
2024-02-24 13:22:37 +0330

    some small changes

--- blog/admin.py
+++ blog/admin.py
@@ -7,7 +7,7 @@ class PostAdmin(admin.ModelAdmin):
     date_hierarchy = 'created_date'
     empty_value_display = "-empty-"
     list_display = ('title', 'author', 'status', 'published_date', 'login_required')
-    list_filter = ('author','categories')
+    list_filter = ('author','categories','tags')
     search_fields = ('name', 'subject' , 'email')
 
 
